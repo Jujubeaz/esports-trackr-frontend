7d424d015fea8220c58eaaae71ddafb5afb0db38
JSiow <julian.k.siow@gmail.com>
2019-12-06 09:07:46 -0800

    connected navbar to home and styled league page

--- src/components/League/League.js
+++ src/components/League/League.js
@@ -12,23 +12,32 @@ class League extends Component {
   }
 
   componentDidMount() {
-    this.findFilter();
+    this.findData();
   }
 
-  findFilter = () => {
+  setSplash = (img) => {
+    const splashImg = document.querySelector("div.hero")
+    splashImg.style.backgroundImage = `url("${img}")`
+  }
+
+  findData = () => {
     if(this.props.match.params.leagueId == 1){
+      this.setSplash("https://es.me/wp-content/uploads/2017/12/NA-LCS-format-1-1.jpg")
       this.setState({
         filter: [203, 4]
       }, this.getTeams())
     } else if (this.props.match.params.leagueId == 2){
+      this.setSplash("https://l2pbomb.com/wp-content/uploads/2018/09/League-Of-Legends-LCK-LOL-Park-1366x768.jpeg")
       this.setState({
         filter: [107]
       }, this.getTeams())
     } else if (this.props.match.params.leagueId == 3){
+      this.setSplash("https://esportsobserver.com/wp-content/uploads/2018/12/20181201014016.jpg")
       this.setState({
         filter: [49]
       }, this.getTeams())
     } else if (this.props.match.params.leagueId == 4){
+      this.setSplash("https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/02/13002920/12.jpg")
       this.setState({
         filter: [184, 176, 272, 61, 80, 76, 7]
       }, this.getTeams())
