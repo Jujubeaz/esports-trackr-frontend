d080b1aebe529460b6bd5db5dafa158d7ea27639
JSiow <julian.k.siow@gmail.com>
2019-12-08 14:17:54 -0800

    adds match page and requests for match page

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home/Home';
 import League from '../components/League/League';
 import Profile from '../components/Profile/Profile';
+import Match from '../components/Match/Match';
 import NotFound from '../components/NotFound/NotFound';
 
 export default ({ currentUser, setCurrentUser, filter }) => (
@@ -11,6 +12,7 @@ export default ({ currentUser, setCurrentUser, filter }) => (
       <Route exact path='/' render={() => <Home currentUser={currentUser} setCurrentUser={setCurrentUser}/>} />
       <Route path="/league/:leagueId" render={() => <League filter={filter}/>} />
       <Route path="/team/:teamId" render={() => <Profile />} />
+      <Route path="/match/:matchId" component={Match} />
       <Route path='/' component={NotFound} />
     </Switch>
   </>
